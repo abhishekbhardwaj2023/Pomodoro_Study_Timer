10ed89db335c6d063b64e03c76536e642c224d58
ABHISHEK BHARDWAJ <59165472+abhishekbhardwaj2023@users.noreply.github.com>
2021-12-11 00:20:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,20 @@
 from tkinter import *
 import math
 # ---------------------------- CONSTANTS ------------------------------- #
+
+# Colors
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
+
 FONT_NAME = "Courier"
+
+# Time
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+
 reps = 0
 timer = None
 
